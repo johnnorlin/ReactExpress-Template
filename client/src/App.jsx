492c560b5f4a0bcf2c56af6651f3c2c9a152794d
johnnorlin <johnnorlinmusic@gmail.com>
2023-12-21 18:13:37 -0600

    React/Express App Template

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles.css";
+
+function App() {
+  return <div id="AppContainer"></div>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+//Parse JSON incoming
+app.use(express.json());
+
+//Accept requests from any domain - to be updated
+app.use(cors({ origin: "*" }));
+
+//serve static files and the index.html file
+app.use("/", express.static(path.join(__dirname, "../client")));
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/index.html"));
+});
+
+//serve 404 error to all other unknown routes
+app.use("*", (req, res) => res.status(404).send("Page not found"));
+
+//global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "Express error handler caught an unknown middlware error, uh-oh!",
+    status: 500,
+    message: { err: "An error occured" },
+  };
+  //use default err mashed with changes from passed in err
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).send(errorObj.message);
+});
+
+//listen for port
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
